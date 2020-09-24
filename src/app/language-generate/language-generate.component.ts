6d86daf4a50058ed9d14d1c0ac54d32eb342c780
upputollahari <hariu755@gmail.com>
2020-09-24 16:29:50 +0530

    2 files modified

--- src/app/language-generate/language-generate.component.ts
+++ src/app/language-generate/language-generate.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Component, OnChanges,  OnInit, ViewChild,  ViewEncapsulation } from "@angular/core";
 import { FormControl } from "@angular/forms";
+import { MatTabChangeEvent, MatTabGroup } from "@angular/material/tabs";
 
 @Component({
   selector: "app-language-generate",
@@ -10,26 +11,29 @@ import { FormControl } from "@angular/forms";
 })
 export class LanguageGenerateComponent implements OnInit {
   // tabs = ["First", "Second", "Third"];
-  indexValue;
-  selected = new FormControl(0);
+ 
+  indexValue=0;
+  // selected = new FormControl(1);
+  dataSource=['name','pos'];
   data = {
     "en-US":{
-        "wrapper":{
+        "wrapper1":{
             "ticketpurchased": "Tickets purchased successfully",
             "gotfree": "Free tickets you got.",
             "maxTicketsPurchased": "Maximum tickets purchased."
         },
-        "game":{
+        "game1":{
             "jackpotId": "Jackpot ID",
             "payout": "Payout",
             "winTrigger": "Win trigger",
             "winnerOrCommunity": "Winner / Community"
         },
-        "chat":{
+        "chat1":{
             "chat_skill_level": "Chat skill level: %s",
             "pending_friends": "(Pending friends: %s)",
             "no_buddies": "No buddies found."
         }
+ 
     },
     "pt-BR":{
         "wrapper2":{
@@ -52,22 +56,30 @@ export class LanguageGenerateComponent implements OnInit {
 };
   tabs=[];
   subtabs=[];
+  tabChanged = (tabChangeEvent: MatTabChangeEvent): void => {
+    // console.log('tabChangeEvent => ', tabChangeEvent);
+    // console.log('index => ', tabChangeEvent.index);
+    this.indexValue=tabChangeEvent.index;
+    this.display();
+
+  }
   constructor() {}
 
+// ngOnChanges(){
+//   console.log("index value :"+ this.indexValue);
+// }
   ngOnInit() {
   this.tabs=Object.keys(this.data).filter(data=>{
     return data;
   });
   this.display();
-  // console.log("main tabs : " ,this.tabs);
-  // console.log("sub tabs : " ,this.subtabs);
-  console.log(" index is : "+ this.selected.value);
-  console.log(this.selected.value);
   }
+
   display(){
-    // console.log(" index is : "+ this.selected.value);
-  Object.keys(this.data[this.tabs[this.selected.value]]).filter(data=>{
-    this.subtabs.push(data)
+    console.log(" index is : "+ this.indexValue);
+    this.subtabs=Object.keys(this.data[this.tabs[this.indexValue]]).filter(data=>{
+      return data;
   });
+  console.log(this.subtabs);
   }
 }
